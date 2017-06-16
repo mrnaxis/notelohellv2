99649c99d88cacc7c151a42c8569e492ddfbb968
Guilherme Montosa <guilherme@datawash.com.br>
2017-06-16 16:14:32 -0300

    Add project files.

---
+++ notelohell/Controllers/AuthController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace notelohell.Controllers
+{
+    public class AuthController : Controller
+    {
+        // GET: Auth
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Registro()
+        {
+            return View();
+        }
+    }
+}
---
+++ notelohell/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="notelohell.MvcApplication" Language="C#" %>
