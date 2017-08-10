91316b215adf41b4cf09d3f6d8f1f59cf469a0d6
Jefferson Tracinkas <jefferson.tracinkas@gmail.com>
2017-08-10 00:13:37 -0300

    adicionando models e dao para trabalhar com mongodb

---
+++ notelohell/App_Start/MongoConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Threading.Tasks;
+
+namespace notelohell.App_Start
+{
+    public class MongoConfig
+    {
+        protected static IMongoClient _client;
+        protected static IMongoDatabase _database;
+
+        public MongoConfig()
+        {
+            _client = new MongoClient();
+            _database = _client.GetDatabase("notel");
+        }
+        public async void salvarCollection(BsonDocument doc,string collection)
+        {
+           var col = _database.GetCollection<BsonDocument>(collection);
+            await col.InsertOneAsync(doc);
+        }
+    }
+}
---
+++ notelohell/DAO/UsersDAO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Threading.Tasks;
+using notelohell.Models;
+using notelohell.App_Start;
+
+namespace notelohell.DAO
+{
+    public class UsersDAO
+    {
+        public void gravarUsuario(UsersModel user)
+        {
+            var usuario = new BsonDocument
+            {
+                {"nickname",user.Id },
+                {"email",user.Email },
+                {"pwhash",user.pwHash },
+                {"gametag",user.gameTag }
+            };
+            MongoConfig conf = new MongoConfig();
+            conf.salvarCollection(usuario,"usuarios");
+        }
+    }
+}
