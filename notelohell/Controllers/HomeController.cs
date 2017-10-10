7765314b2a384d73e43dc5495943ecbc311aa23f
Guilherme Montosa <guilherme@datawash.com.br>
2017-10-10 17:38:13 -0300

    exemplo do exemplo do exemplo de visualização dos dados usando o google charts

--- notelohell/Controllers/HomeController.cs
+++ notelohell/Controllers/HomeController.cs
@@ -46,7 +46,7 @@ namespace notelohell.Controllers
                     wc.Headers.Add("Accept-Language", " en-US");
                     wc.Headers.Add("Accept", " text/html, application/xhtml+xml, */*");
                     wc.Headers.Add("User-Agent", "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)");
-                    string url = "http://owapi.net/api/v3/u/" + tag + "/stats";
+                    string url = "http://owapi.net/api/v3/u/" + tag + "/heroes";
                     data = wc.DownloadString(url);
                 }
             }
