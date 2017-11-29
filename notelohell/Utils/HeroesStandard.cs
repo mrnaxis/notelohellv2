be54425e92d84d7a372ee63cd856cad025616b8d
Guilherme Montosa <guilherme.montosa@fatec.sp.gov.br>
2017-11-29 00:55:42 -0200

    Adição de classe, ajutes em tela de estatisticas

---
+++ notelohell/Utils/HeroesStandard.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace notelohell.Utils
+{
+    public class HeroesStandard
+    {
+        public static IList<string> Categories = 
+            new List<string>()
+            {
+                "Offensive",
+                "Defensive",
+                "Tank",
+                "Support"
+            };
+
+        public static IDictionary<string, string> Heroes = new Dictionary<string, string>()
+        {
+
+        };
+    }
+}
